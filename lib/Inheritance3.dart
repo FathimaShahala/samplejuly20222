4fc5cc8ea0c523175e6c363154715aaad0a9c180
user <shahala.rafeek123@gmail.com>
2022-09-18 23:01:26 +0530

    commit

--- lib/Inheritance3.dart
+++
@@ -1,9 +0,0 @@
-class bag{
-  void details(String Model , int Year , String Engine , double Milage){
-    print('Model    = $Model');
-    print('Year     = $Year');
-    print('Engine   = $Engine');
-    print('Milage   = $Milage');
-  }
-
-  NOT COMPLETED
