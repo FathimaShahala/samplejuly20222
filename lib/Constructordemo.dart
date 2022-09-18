2f46ee589289c0716d652f0216d4ebe2830a93a1
user <shahala.rafeek123@gmail.com>
2022-09-18 22:36:36 +0530

    commit

--- lib/Constructordemo.dart
+++ lib/Constructordemo.dart
@@ -1,17 +1,18 @@
-class Car(){
-  //Car(){}//default consstructor
-Car(String data){
-  print('data = $data');
-}
-Car.a(int a, int b){
-  print('SUM =${a+b}');
-}
-Car.b(String c, in t a){
-  print('my name is $c am $a yrs old');
-}
+class Car{
+  // Car(){ }// default constructor
+  Car(String data){
+    print('data = $data');
+  }
+  Car.a(int a, int b){
+    print('SUM = ${a+b}');
+  }
+  Car.b(String c,int a){
+    print('My name is $c am $a yrs old');
+  }
 }
 void main(){
-  var obj    = Car("Hello");
-  var obj1   = Car.a(45,65);
-  var obj2   = Car.b('Anu',23);
+  // classname object = classname();
+  var obj   =  Car("Hello");
+  var obj1  =  Car.a(45, 65);
+  var obj2  =  Car.b('Anu',23);
 }
